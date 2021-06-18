3ddb150513362eb342957be4678b648ee46afefb
Marius Lauff <marius@lauff.com>
2021-06-18 21:16:56 +0200

    Schönheit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'GradesMain.dart';
 
 /*
  * Main-Klasse der App, diese wird NICHT benötigt, diese Klasse verweist lediglich auf die Main-Klasse des Notenbereiches (GradesMain.dart) für ein einfacheres Zusammenführen
  */
 
-void main()=>runApp(new MyApp());
-
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
-      title:'ESSS - App',
+      title: 'ESSS - App',
       home: MainWidget(),
     );
   }
@@ -28,36 +26,31 @@ class MainWidget extends StatelessWidget {
         title: Text("ESS - App"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text("Hier ist die Main-Klasse, über die später auf den Notenbereich verwiesen werden kann.\n"
-                "Der Name der Main-Klasse für den Notenbereich ist \"GradesMain.dart\" => \"GradeApp()\"\n\n"
-                "Achten Sie beim zusammenführen darauf, dass in der \"pubspec.yaml\"-Date folgende Änderungen vorgenommen wurden:\n\n"
-                "dependencies:\n"
-                "  json_annotation: ^3.0.1\n"
-                "  shared_preferences: ^0.5.12+4\n"
-                "  http: ^0.12.2\n"
-                "  encrypt: ^4.1.0\n\n"
-                "dev_dependencies:\n"
-                "  json_serializable: ^3.3.0"),
-            FlatButton(
-              child: Text("Zum Notenbereich"),
-              onPressed: () => navigateToGradesApp(context),
-            ),
-          ],
-        )
-      ),
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Text(
+                  "Hier ist die Main-Klasse, über die später auf den Notenbereich verwiesen werden kann.\n"
+                  "Der Name der Main-Klasse für den Notenbereich ist \"GradesMain.dart\" => \"GradeApp()\"\n\n"
+                  "Achten Sie beim zusammenführen darauf, dass in der \"pubspec.yaml\"-Date folgende Änderungen vorgenommen wurden:\n\n"
+                  "dependencies:\n"
+                  "  json_annotation: ^3.0.1\n"
+                  "  shared_preferences: ^0.5.12+4\n"
+                  "  http: ^0.12.2\n"
+                  "  encrypt: ^4.1.0\n\n"
+                  "dev_dependencies:\n"
+                  "  json_serializable: ^3.3.0"),
+              TextButton(
+                child: Text("Zum Notenbereich"),
+                onPressed: () => navigateToGradesApp(context),
+              ),
+            ],
+          )),
     );
   }
 
   void navigateToGradesApp(BuildContext context) {
     Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => new GradeApp())
-    );
+        context, MaterialPageRoute(builder: (context) => new GradeApp()));
   }
 }
-
-
-
