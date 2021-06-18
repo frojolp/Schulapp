db5be1cec5e5dbddffab53f98360dd4725447e25
Marius Lauff <marius@lauff.com>
2021-06-18 22:29:01 +0200

    Hochladen verknüpfung und neue Dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -77,7 +77,7 @@ class TodoApp extends StatelessWidget {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (BuildContext context) => GradesMain(),
+                          builder: (BuildContext context) => runApp(GradesMain),
                         ))
                   },
                   color: esssblau,
@@ -98,7 +98,7 @@ class TodoApp extends StatelessWidget {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (BuildContext context) => GradesMain(),
+                          builder: (BuildContext context) => runApp(GradesMain),
                         ))
                   },
                   color: esssblau,
@@ -119,7 +119,7 @@ class TodoApp extends StatelessWidget {
                     Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (BuildContext context) => GradesMain(),
+                          builder: (BuildContext context) => runApp(GradesMain),
                         ))
                   },
                   color: esssblau,
@@ -141,7 +141,7 @@ class TodoApp extends StatelessWidget {
                                 context,
                                 MaterialPageRoute(
                                   builder: (BuildContext context) =>
-                                      GradesMain(),
+                                      runApp(GradesMain),
                                 ))
                           },
                       color: esssblau,
@@ -192,7 +192,7 @@ class TodoApp extends StatelessWidget {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (BuildContext context) => GradesMain(),
+                      builder: (BuildContext context) => runApp(GradesMain),
                     ));
               },
             ),
@@ -216,7 +216,7 @@ class TodoApp extends StatelessWidget {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (BuildContext context) => GradesMain(),
+                      builder: (BuildContext context) => runApp(GradesMain),
                     ));
               },
             ),
@@ -240,7 +240,7 @@ class TodoApp extends StatelessWidget {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (BuildContext context) => GradesMain(),
+                      builder: (BuildContext context) => runApp(GradesMain),
                     ));
               },
             ),
@@ -264,7 +264,7 @@ class TodoApp extends StatelessWidget {
                 Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (BuildContext context) => GradesMain(),
+                      builder: (BuildContext context) => runApp(GradesMain),
                     ));
               },
             ),
@@ -275,59 +275,5 @@ class TodoApp extends StatelessWidget {
         ),
       ),
     ));
-  }
-}
-
-/*
- * Main-Klasse der App, diese wird NICHT benötigt, diese Klasse verweist lediglich auf die Main-Klasse des Notenbereiches (GradesMain.dart) für ein einfacheres Zusammenführen
- */
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'ESSS - App',
-      home: MainWidget(),
-    );
-  }
-}
-
-class MainWidget extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("ESS - App"),
-      ),
-      body: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              Text(
-                  "Hier ist die Main-Klasse, über die später auf den Notenbereich verwiesen werden kann.\n"
-                  "Der Name der Main-Klasse für den Notenbereich ist \"GradesMain.dart\" => \"GradeApp()\"\n\n"
-                  "Achten Sie beim zusammenführen darauf, dass in der \"pubspec.yaml\"-Date folgende Änderungen vorgenommen wurden:\n\n"
-                  "dependencies:\n"
-                  "  json_annotation: ^3.0.1\n"
-                  "  shared_preferences: ^0.5.12+4\n"
-                  "  http: ^0.12.2\n"
-                  "  encrypt: ^4.1.0\n\n"
-                  "dev_dependencies:\n"
-                  "  json_serializable: ^3.3.0"),
-              TextButton(
-                child: Text("Zum Notenbereich"),
-                onPressed: () => navigateToGradesApp(context),
-              ),
-            ],
-          )),
-    );
-  }
-
-  void navigateToGradesApp(BuildContext context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => new GradeApp()));
   }
 }
