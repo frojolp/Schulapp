2c94dd0e7236693df4a722ad9a14104026a4a7dc
Marius Lauff <marius@lauff.com>
2021-06-19 12:34:48 +0200

    Fertig mit Lehrerliste

--- lib/Lehrerliste.dart
+++ lib/Lehrerliste.dart
@@ -3,13 +3,21 @@ import 'package:flutter_notenapp/models/Lehrer_model.dart';
 import 'package:flutter_notenapp/screens/Lehrer_detail_screen.dart';
 import 'package:flutter_notenapp/screens/Lehrer_screen.dart';
 import 'package:flutter_notenapp/screens/unknown_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_notenapp/main.dart';
+import 'GradesMain.dart';
+
+Color esssblau = Color(0xff3d6795);
+Color esssrot = Color(0xffe23c4e);
+const _url = 'https://www.esss.de/news';
 
 class App extends StatefulWidget {
   @override
-  _AppState createState() => _AppState();
+  _Lehrerliste createState() => _Lehrerliste();
 }
 
-class _AppState extends State<App> {
+class _Lehrerliste extends State<App> {
   Lehrer _selectedLehrer;
   final bool showUnknown = false;
   List<Lehrer> lehrer = Lehrkraefte;
@@ -42,12 +50,12 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Lehrerliste',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Lehrerliste', style: GoogleFonts.baiJamjuree()),
+          backgroundColor: esssblau,
         ),
-        home: Navigator(
+        body: Navigator(
           onPopPage: this.onPopPage,
           pages: [
             MaterialPage(
@@ -67,6 +75,126 @@ class _AppState extends State<App> {
                   key: ValueKey('SelectedLehrer'),
                   child: LehrerDetailsscreen(lehrer: this._selectedLehrer))
           ],
-        ));
+        ),
+
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                          'https://www.esss.de/wp-content/uploads/2019/01/Slider_Schulgeb%C3%A4ude.jpg')),
+                  color: esssrot,
+                ),
+                child: InkWell(
+                    //onTap: _launchURL,
+                    ),
+              ),
+              Divider(
+                thickness: (1.5),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.calculate_outlined,
+                  color: esssrot,
+                ),
+                title: Text(
+                  'Noten',
+                  style: GoogleFonts.roboto(),
+                ),
+                trailing: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: esssrot,
+                ),
+                onTap: () => navigateToGradesApp(context),
+              ),
+              Divider(
+                thickness: (1.5),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.account_circle,
+                  color: esssrot,
+                ),
+                title: Text(
+                  'Lehrerliste',
+                  style: GoogleFonts.roboto(),
+                ),
+                trailing: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: esssrot,
+                ),
+                onTap: () => navigateToLehrerApp(context),
+              ),
+              Divider(
+                thickness: (1.5),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.alternate_email,
+                  color: esssrot,
+                ),
+                title: Text(
+                  'Linkliste',
+                  style: GoogleFonts.roboto(),
+                ),
+                trailing: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: esssrot,
+                ),
+                onTap: () => navigateToGradesApp(context),
+              ),
+              Divider(
+                thickness: (1.5),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.assignment_outlined,
+                  color: esssrot,
+                ),
+                title: Text(
+                  'To-Do Liste',
+                  style: GoogleFonts.roboto(),
+                ),
+                trailing: Icon(
+                  Icons.keyboard_arrow_right,
+                  color: esssrot,
+                ),
+                onTap: () => navigateToGradesApp(context),
+              ),
+              Divider(
+                thickness: (1.5),
+              ),
+            ],
+          ),
+        ),
+        //theme: ThemeData(
+        //  primarySwatch: Colors.blue,
+        //  visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home:
+    );
   }
+}
+
+void navigateToLehrerApp(BuildContext context) {
+  Navigator.push(context, MaterialPageRoute(builder: (context) => new App()));
+}
+
+void navigateToGradesApp(BuildContext context) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => new GradeApp()));
+}
+
+void navigateToLinkApp(BuildContext context) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => new GradeApp()));
+}
+
+void navigateToTodoApp(BuildContext context) {
+  Navigator.push(
+      context, MaterialPageRoute(builder: (context) => new GradeApp()));
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter_notenapp/Lehrerliste.dart';
 
 Color esssblau = Color(0xff3d6795);
 Color esssrot = Color(0xffe23c4e);
@@ -88,7 +89,7 @@ class TodoApp extends StatelessWidget {
                 child: Center(
                     child: CupertinoButton(
                   minSize: 250,
-                  onPressed: () => navigateToGradesApp(context),
+                  onPressed: () => navigateToLehrerApp(context),
                   color: esssblau,
                   child: Text("Lehrerliste",
                       textAlign: TextAlign.center,
@@ -181,7 +182,7 @@ class TodoApp extends StatelessWidget {
                 Icons.keyboard_arrow_right,
                 color: esssrot,
               ),
-              onTap: () => navigateToGradesApp(context),
+              onTap: () => navigateToLehrerApp(context),
             ),
             Divider(
               thickness: (1.5),
@@ -229,8 +230,7 @@ class TodoApp extends StatelessWidget {
   }
 
   void navigateToLehrerApp(BuildContext context) {
-    Navigator.push(
-        context, MaterialPageRoute(builder: (context) => new GradeApp()));
+    Navigator.push(context, MaterialPageRoute(builder: (context) => new App()));
   }
 
   void navigateToGradesApp(BuildContext context) {
